45af1e839a9969352ca5618aecf05e0f66b338a9
alicedev94 <alicedev94@gmail.com>
2025-06-13 15:45:55 -0400

    clean project

--- src/layout/AppTopbar.vue
+++ src/layout/AppTopbar.vue
@@ -39,16 +39,6 @@ const { toggleMenu, toggleDarkMode, isDarkTheme } = useLayout();
                 <button type="button" class="layout-topbar-action" @click="toggleDarkMode">
                     <i :class="['pi', { 'pi-moon': isDarkTheme, 'pi-sun': !isDarkTheme }]"></i>
                 </button>
-                <div class="relative">
-                    <button
-                        v-styleclass="{ selector: '@next', enterFromClass: 'hidden', enterActiveClass: 'animate-scalein', leaveToClass: 'hidden', leaveActiveClass: 'animate-fadeout', hideOnOutsideClick: true }"
-                        type="button"
-                        class="layout-topbar-action layout-topbar-action-highlight"
-                    >
-                        <i class="pi pi-palette"></i>
-                    </button>
-                    <AppConfigurator />
-                </div>
             </div>
 
             <button
@@ -57,23 +47,6 @@ const { toggleMenu, toggleDarkMode, isDarkTheme } = useLayout();
             >
                 <i class="pi pi-ellipsis-v"></i>
             </button>
-
-            <div class="layout-topbar-menu hidden lg:block">
-                <div class="layout-topbar-menu-content">
-                    <button type="button" class="layout-topbar-action">
-                        <i class="pi pi-calendar"></i>
-                        <span>Calendar</span>
-                    </button>
-                    <button type="button" class="layout-topbar-action">
-                        <i class="pi pi-inbox"></i>
-                        <span>Messages</span>
-                    </button>
-                    <button type="button" class="layout-topbar-action">
-                        <i class="pi pi-user"></i>
-                        <span>Profile</span>
-                    </button>
-                </div>
-            </div>
         </div>
     </div>
 </template>
--- vite.config.mjs
+++ vite.config.mjs
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
 
+import vueDevTools from 'vite-plugin-vue-devtools';
+
 // https://vitejs.dev/config/
 export default defineConfig({
     optimizeDeps: {
@@ -12,6 +14,7 @@ export default defineConfig({
     },
     plugins: [
         vue(),
+        vueDevTools(),
         Components({
             resolvers: [PrimeVueResolver()]
         })
