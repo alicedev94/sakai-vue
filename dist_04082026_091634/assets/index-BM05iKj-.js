de7f0c7beb890bd118b17fcda7f366cbb036cb22
Gokoshijr <julioagonzalez18@gmail.com>
2026-08-11 17:15:50 -0300

    chore: limpiar backups de dist colados en el repo

--- dist_04082026_091634/assets/index-BM05iKj-.js
+++
@@ -1,1 +0,0 @@
-import{b9 as l,ab as o,ba as a}from"./index-18hazH2F.js";function b(){l({variableName:o("scrollbar.width").name})}function c(){a({variableName:o("scrollbar.width").name})}export{b,c as u};
--- dist_dilan_20260806/assets/index-lcW06Tl_.js
+++
@@ -1,1 +0,0 @@
-import{ba as l,ac as o,bb as a}from"./index-CwxAHeh0.js";function r(){l({variableName:o("scrollbar.width").name})}function b(){a({variableName:o("scrollbar.width").name})}export{r as b,b as u};
